86395e6c73ce5be394f0f5bfd299618db77bf5f0
Al-Homede-1 <155636135+Al-Homede-1@users.noreply.github.com>
2024-06-21 15:06:19 +0300

    adding the new page

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,12 @@ import "./index.css";
 import App from "./App";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <Navbar />
     <Header />
     <Footer />
     <App />
