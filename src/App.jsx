cea017c7b55d33b9964daad06ffbba8bbe8b9940
Al-Homede-1 <155636135+Al-Homede-1@users.noreply.github.com>
2024-06-22 00:53:19 +0300

    adding new functions in page app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import React from "react";
+import { CiStar } from "react-icons/ci";
+import { FaStar } from "react-icons/fa";
 export default function App() {
+  const [moh, setmoh] = React.useState(true);
+  function tog() {
+    setmoh(v => !v)
+  }
   return (
-    <div>
-      <h1>hi</h1>
-      <h4>my name is</h4>
-      <h1>MOHAMMED</h1>
+    <div style={{ fontSize: "40px", textAlign: "center" }} onClick={tog}>
+      {moh ? "yes" : "No"}
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Navbar />
-    <Header />
-    <Footer />
-    <App />
-  </React.StrictMode>
+    <div className="">
+      <App />
+    </div> 
 );
