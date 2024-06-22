0730a0e981a47f1b29049e74c90fb9b05f80cf3b
Al-Homede-1 <155636135+Al-Homede-1@users.noreply.github.com>
2024-06-23 00:36:24 +0300

    adding new functions in page app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import React from "react";
-import { CiStar } from "react-icons/ci";
-import { FaStar } from "react-icons/fa";
+import React, {useEffect, useState } from "react";
+import Name from "./components/name";
+
 export default function App() {
-  const [moh, setmoh] = React.useState(true);
-  function tog() {
-    setmoh(v => !v)
-  }
+  const [showdata, setshowdata] = useState([]);
+
+  const datashow = showdata.map((item) => <Name name={item}/>)
+
+  useEffect(() => {
+    fetch('https://api.imgflip.com/get_memes')
+    .then((res) => res.json())
+    .then((data) => setshowdata(data.data.memes.map((e) => e.name)));
+  });
   return (
-    <div style={{ fontSize: "40px", textAlign: "center" }} onClick={tog}>
-      {moh ? "yes" : "No"}
-    </div>
+    <div>{datashow}</div>
   );
+  
 }
