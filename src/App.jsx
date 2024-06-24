4ed1a9fc0b269e3da4924d480899384a08ecf95f
Al-Homede-1 <155636135+Al-Homede-1@users.noreply.github.com>
2024-06-24 09:44:25 +0300

    ching a size of text in sidbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,9 @@
 import React, {useEffect, useState } from "react";
+// 'https://api.imgflip.com/get_memes'
 import Name from "./components/name";
 
 export default function App() {
-  const [showdata, setshowdata] = useState([]);
 
-  const datashow = showdata.map((item) => <Name name={item}/>)
-
-  useEffect(() => {
-    fetch('https://api.imgflip.com/get_memes')
-    .then((res) => res.json())
-    .then((data) => setshowdata(data.data.memes.map((e) => e.name)));
-  });
-  return (
-    <div>{datashow}</div>
-  );
+  return;
   
 }
